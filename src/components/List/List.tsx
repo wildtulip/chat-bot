46f262d47a1a99c4eca5c7b37eeb1eeeaf3669c7
Denis Kovalev <denis.kovalev@wtt-solutions.com>
2021-04-20 11:36:11 +0300

    add minpmap draw

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,16 +4,18 @@ import styles from './List.module.scss'
 import ReactFlow, {
   removeElements,
   addEdge,
-  MiniMap,
-  Controls,
-  Background,
+  // MiniMap,
+  // Controls,
+  // Background,
   Elements,
   OnLoadParams,
   Edge,
   Connection,
+  Position,
+  ArrowHeadType,
 } from 'react-flow-renderer';
-import { initialElements } from './initial-elements';
-import { BotStep } from '../../types/types';
+
+import { BotStep, BotStepLinked } from '../../types/types';
 
 const onLoad = (reactFlowInstance: OnLoadParams) => {
   console.log('flow loaded:', reactFlowInstance);
@@ -22,57 +24,145 @@ const onLoad = (reactFlowInstance: OnLoadParams) => {
 
 const List = () => {
   // REQUEST
-  const [data, setData] = useState<BotStep[]>([]);
+  const [data, setData] = useState<BotStepLinked[]>([]);
+  const [initialElements, setInitialElements] = useState<Elements>([]);
+  let [counter, setCounter] = useState<number>(0);
+
   useEffect(() => {
     axios.get('https://api.host1.dev.webxid.net/1.0/bots/227/steps?search_at=name&query=&page=1')
       .then(function (response) {
         setData(response.data.data.map((step: BotStep) => {
-            step.linked_steps = step.linked_steps.map((id) => response.data.data.find((step: BotStep) => step.step_id.toString() === id));
-            return step;
+          step.linked_steps = step.linked_steps.map((id) => response.data.data.find((step: BotStep) => step.step_id.toString() === id));
+          return step;
         }))
+
       })
       .catch(function (error) {
         console.log(error);
       })
 
   }, []);
-  console.log(data);
+
+  useEffect(() => {
+    let elementsToRender: Elements = [];
+
+    const calculate = (step: BotStepLinked) => {
+      if (typeof (step.depth_level) === 'number') {
+        return
+      }
+      if (step.parent_step_id === 0) {
+        step.depth_level = 0;
+        return
+      }
+      const parent = data.find((maybeParent) => step.parent_step_id === maybeParent.step_id)!
+      if (typeof (parent.depth_level) !== 'number') {
+        calculate(parent) 
+      }
+      step.depth_level = parent.depth_level! + 1
+    }
+
+    const verticalOffset = (step: BotStepLinked) => {
+      if (step.parent_step_id === 0) {
+        setCounter(counter++);
+        step.vertical_offset = counter;
+        return
+      }
+      const parent = data.find((maybeParent) => step.parent_step_id === maybeParent.step_id)!
+      const index = parent.linked_steps.findIndex((linkedStep) => step.step_id === linkedStep.step_id);
+      step.vertical_offset = index + 1;
+    }
+
+    for (const step of data) {
+      if (step.parent_step_id === null || step.step_id === null) {
+        continue
+      }
+      calculate(step);
+      verticalOffset(step);
+      elementsToRender.push({
+        id: step.step_id.toString(),
+        position: { x: 270 * (step.depth_level! + 1), y: 70 * step.vertical_offset! },
+        type: step.depth_level === 0 ? 'input' : 'default',
+        data: {
+          label: (<><span><b>{step.name}</b></span><br /><span>/{step.command_key}</span></>)
+        },
+        style: {
+          height: 30,
+          width: 130,
+        },
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left
+      })
+
+    }
+    for (const step of data) {
+      if (step.parent_step_id === null || step.step_id === null || step.parent_step_id === 0) {
+        continue
+      }
+      elementsToRender.push({
+        id: `e${step.parent_step_id}-${step.step_id}`,
+        source: step.parent_step_id.toString(),
+        target: step.step_id.toString(),
+        arrowHeadType: ArrowHeadType.ArrowClosed
+      })
+
+    }
+    console.log(data);
+
+
+
+    setInitialElements(elementsToRender);
+
+  }, [data])
+
+
+
+  console.log(initialElements);
+
+
+
+  // {
+  //   id: '590',
+  //   type: 'input',
+  //   data: {
+  //     label: (
+  //       <>
+  //         <strong>start</strong><br />
+  //         <span>/start</span>
+  //       </>
+  //     ),
+  //   },
+  //   position: { x: 400, y: 0 },
+  //   style: {
+  //     height: 30,
+  //     width: 130,
+  //   },
+  // },
+
+
+
 
 
   // REACT FLOW SETTINGS
-  const [elements, setElements] = useState<Elements>(initialElements);
+  // const [elements, setElements] = useState<Elements>(initialElements);
   const onElementsRemove = (elementsToRemove: Elements) =>
-    setElements((els) => removeElements(elementsToRemove, els));
-  const onConnect = (params: Edge | Connection) => setElements((els) => addEdge(params, els));
+    setInitialElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params: Edge | Connection) => setInitialElements((els) => addEdge(params, els));
+  // console.log({ elements });
+
 
   return (
     <div className={styles.wrapper}>
       <ReactFlow
-        elements={elements}
+        elements={initialElements}
         onElementsRemove={onElementsRemove}
         onConnect={onConnect}
         onLoad={onLoad}
         snapToGrid={true}
         snapGrid={[15, 15]}
       >
-        <MiniMap
-          nodeStrokeColor={(n) => {
-            if (n.style?.background) return `${n.style.background}`;
-            if (n.type === 'input') return '#0041d0';
-            if (n.type === 'output') return '#ff0072';
-            if (n.type === 'default') return '#1a192b';
 
-            return '#eee';
-          }}
-          nodeColor={(n) => {
-            if (n.style?.background) return `${n.style.background}`;
-
-            return '#fff';
-          }}
-          nodeBorderRadius={2}
-        />
-        <Controls />
-        <Background color="#aaa" gap={16} />
+        {/* <Controls /> */}
+        {/* <Background color="#aaa" gap={24} /> */}
       </ReactFlow>
       {data.map((el, i) => <div key={i} className={styles.listItem}>
         <h3>{el.step_id}</h3>
--- src/types/types.ts
+++ src/types/types.ts
@@ -5,5 +5,19 @@ export type BotStep = {
   type_id: number,
   sort: number,
   linked_steps: string[],
-  bot_id: number
+  bot_id: number,
+  parent_step_id: number
+}
+
+export type BotStepLinked = {
+  step_id: number,
+  command_key: string,
+  name: string,
+  type_id: number,
+  sort: number,
+  linked_steps: BotStepLinked[],
+  bot_id: number,
+  parent_step_id: number,
+  depth_level?: number,
+  vertical_offset?: number
 }
